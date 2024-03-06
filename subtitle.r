060147855712e27f8bb39f24c756a9f34519fd58
shalini192224028 <123437387+shalini192224028@users.noreply.github.com>
2024-03-06 19:47:30 +0530

    Create subtitle.r

---
+++ subtitle.r
@@ -0,0 +1,18 @@
+library(ggplot2)  
+# Giving a name to the chart file.  
+png(file = "scatterplot6.png")  
+#Creating scatterplot with fitted values.  
+# An additional function stst_smooth is used for linear regression.  
+new_graph<-ggplot(mtcars, aes(x = log(mpg), y = log(drat))) +geom_point(aes(color = factor(gear))) +  
+  stat_smooth(method = "lm",col = "#C42126",se = FALSE,size = 1)  
+#in above example lm is used for linear regression and se stands for standard error.  
+#Adding title with dynamic name  
+new_graph + labs(  
+  title =  
+    "Relation between Mile per hours and drat",  
+  subtitle =  
+    "Relationship break down by gear class",  
+  caption = "Authors own computation"  
+)  
+# Saving the file.  
+dev.off()  
