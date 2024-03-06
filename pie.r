d3fd3876f6278f690d88ebee41aa1185471de55f
shalini192224028 <123437387+shalini192224028@users.noreply.github.com>
2024-03-06 19:43:20 +0530

    Create pie.r

---
+++ pie.r
@@ -0,0 +1,8 @@
+x <- c(20, 65, 15, 50)  
+labels <- c("India", "America", "Shri Lanka", "Nepal")  
+# Giving the chart file a name.  
+png(file = "Country.jpg")  
+# Plotting the chart.  
+pie(x,labels)  
+# Saving the file.  
+dev.off()  
